280a8ce466cae293daa7ff70ef5d24d06ec9bfa1
grammenoudis <giorgosgrammenoudis@gmail.com>
2022-10-28 23:50:15 +0300

    fix lsp capabilities

--- lua/completion-cmp/init.lua
+++ lua/completion-cmp/init.lua
@@ -1,88 +1,91 @@
 local lspconfig = require('lspconfig')
 require("mason").setup({
-  ui = {icons = {package_installed = "✓", package_pending = "➜", package_uninstalled = "✗"}}
+	ui = { icons = { package_installed = "✓", package_pending = "➜", package_uninstalled = "✗" } }
 })
 require("mason-lspconfig").setup()
 -- require("luasnip.loaders.from_vscode").lazy_load()
 local cmp = require 'cmp'
-local select_opts = {behavior = cmp.SelectBehavior.Select}
+local select_opts = { behavior = cmp.SelectBehavior.Select }
 local lspkind = require('lspkind')
 cmp.setup({
-  experimental = {ghost_text = true},
-  formatting = {
-    format = lspkind.cmp_format({
-      mode = 'symbol', -- show only symbol annotations
-      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+	experimental = { ghost_text = true },
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = 'symbol', -- show only symbol annotations
+			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+			ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 
-      -- The function below will be called before any actual modifications from lspkind
-      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      before = function(entry, vim_item)
-        return vim_item
-      end
-    })
-  },
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-    end
-  },
-  window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({select = true}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      local col = vim.fn.col('.') - 1
+			-- The function below will be called before any actual modifications from lspkind
+			-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+			before = function(entry, vim_item)
+				return vim_item
+			end
+		})
+	},
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+		end
+	},
+	window = {
+		-- completion = cmp.config.window.bordered(),
+		-- documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		['<Tab>'] = cmp.mapping(function(fallback)
+			local col = vim.fn.col('.') - 1
 
-      if cmp.visible() then
-        cmp.select_next_item(select_opts)
-      elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        fallback()
-      else
-        cmp.complete()
-      end
-    end, {'i', 's'})
-  }),
-  sources = cmp.config.sources({
-    {name = 'nvim_lsp'}, -- { name = 'vsnip' }, -- For vsnip users.
-    {name = 'luasnip'} -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
-  }, {{name = 'buffer'}})
+			if cmp.visible() then
+				cmp.select_next_item(select_opts)
+			elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+				fallback()
+			else
+				cmp.complete()
+			end
+		end, { 'i', 's' })
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' }, -- { name = 'vsnip' }, -- For vsnip users.
+		{ name = 'luasnip' } -- For luasnip users.
+		-- { name = 'ultisnips' }, -- For ultisnips users.
+		-- { name = 'snippy' }, -- For snippy users.
+	}, { { name = 'buffer' } })
 })
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    {name = 'cmp_git'} -- You can specify the `cmp_git` source if you were installed it.
-  }, {{name = 'buffer'}})
+	sources = cmp.config.sources({
+		{ name = 'cmp_git' } -- You can specify the `cmp_git` source if you were installed it.
+	}, { { name = 'buffer' } })
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({'/', '?'},
-                  {mapping = cmp.mapping.preset.cmdline(), sources = {{name = 'buffer'}}})
+cmp.setup.cmdline({ '/', '?' },
+	{ mapping = cmp.mapping.preset.cmdline(), sources = { { name = 'buffer' } } })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({{name = 'path'}}, {{name = 'cmdline'}})
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({ { name = 'path' } }, { { name = 'cmdline' } })
 })
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 -- See :help mason-lspconfig-dynamic-server-setup
 require('mason-lspconfig').setup_handlers({
-  function(server)
-    lspconfig[server].setup({})
-  end,
-  ['tsserver'] = function()
-    lspconfig.tsserver.setup({settings = {completions = {completeFunctionCalls = true}}})
-  end
+	function(server)
+		lspconfig[server].setup { capabilities = capabilities }
+	end,
+	['tsserver'] = function()
+		lspconfig.tsserver.setup({ settings = { completions = { completeFunctionCalls = true } } })
+	end
 })
